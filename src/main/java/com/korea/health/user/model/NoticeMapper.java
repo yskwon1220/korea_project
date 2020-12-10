d6f08f8266f9c54ab79651060ec75b24917699a8
yskwon1220 <yskwon1220@naver.com>
2020-12-11 02:52:36 +0900

    conflict 수정 후 push

--- src/main/java/com/korea/health/user/model/NoticeMapper.java
+++ src/main/java/com/korea/health/user/model/NoticeMapper.java
@@ -9,6 +9,5 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface NoticeMapper {
 	List<NoticeVO> noticeList();
-
-	Object detail(Integer notice_no);
+	NoticeVO detail(int notice_no);
 }
